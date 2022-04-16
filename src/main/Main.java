e8286cf6d2f5381632659548f73c7bcb0d67bad4
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2022-04-16 16:20:09 +0100

    Created Database connection

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,13 @@
 package main;
 
 import main.controller.ToDoController;
+import main.database.DBConnector;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        DBConnector connector = new DBConnector();
         new ToDoController();
     }
 }
